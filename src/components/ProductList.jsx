91c5e9e51402984af9587723fde3f018f7216a42
Matias Damian Giudice <matiasgiudice309@gmail.com>
2025-12-02 18:42:28 -0300

    feat: mejorar diseño responsive de cards con Bootstrap

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,9 +3,9 @@ import ProductCard from "./ProductCard";
 const ProductList = ({ products }) => {
   return (
     <div className="container">
-      <div className="row">
+      <div className="row g-4 justify-content-center">
         {products.map((p) => (
-          <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4 d-flex justify-content-center">
             <ProductCard product={p} />
           </div>
         ))}
